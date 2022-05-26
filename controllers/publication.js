b2ee27343df1d610808c7fc807d1bdf4cdab7037
psychokore <juju.faure@yahoo.fr>
2022-05-26 12:18:57 +0200

    getonebypostid getallpublication

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,6 +1,7 @@
 const { json } = require('express');
 const fs = require('fs');
-const {createPublication} = require('../repository/publication');
+const publication = require('../repository/publication');
+const {createPublication, getAllPublications, getOnePublicationByPostId} = require('../repository/publication');
 
 exports.publish = async (req,res,next) => {
     if (!req.body.publication){
@@ -28,3 +29,56 @@ exports.publish = async (req,res,next) => {
     
     return res.status(201).json({message: 'Published !'})
 };
+
+exports.modifyPublication = async (req, res) => {
+    
+}
+
+exports.deletePublication = async (req, res) => {
+    
+}
+
+exports.getAllPublications = async (req, res) => {
+    await getAllPublications();
+}
+
+exports.getOnePublicationByPostId = async (req, res) => {
+    const postid = req.params.id;
+    const post = await getOnePublicationByPostId(postid);
+    if (!post) {
+        return res.status(404).json({error: 'No results'})
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+exports.getAllPublications = async (req, res) => {
+    const getAllPublications = await getAllPublications(publication)
+    .then(
+        res.status(200).json(publication)
+    )
+    .catch(
+        (error) => {
+            res.status(500).json({error: 'Internal server error'})
+        }
+    );
+}
+*/
+
--- repository/publication.js
+++ repository/publication.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path');
 const conn = require('./mysql');
 
 
@@ -12,8 +13,43 @@ module.exports = {
                 return resolve(results)
             })
         });
+    },
+    getAllPublications: async publication => {
+        return new Promise (resolve => {
+            conn.query('SELECT p.postid, p.content, p.imageurl, p.create_at, CONCAT (u.firstname, u.lastname) AS authorpseudo FROM publication p JOIN `user` u ON p.authorid = u.userId',[publication], (err, results) => {
+                if (err){
+                    return resolve(null);
+                }
+                return resolve (results)
+        })
+    });
+    },
+    getOnePublicationByPostId: async postid => {
+        return new Promise (resolve => {
+            conn.query('SELECT p.postid, p.content, p.imageurl, p.create_at, CONCAT (u.firstname, u.lastname) AS authorpseudo FROM publication p JOIN `user` u ON p.authorid = u.userId WHERE postid = ? LIMIT 1', [postid], (err, results) => {
+                if (err){
+                    return resolve(null);
+                }
+                if (results.length === 1) 
+                    return resolve(results[0])   
+                return resolve(null)
+            });
+        })
     }
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Récupération des publications avec l'auteur
