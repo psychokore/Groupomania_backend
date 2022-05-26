3a980ea929203ff162e22ae631a3d26215113624
psychokore <juju.faure@yahoo.fr>
2022-05-26 12:22:33 +0200

    ajout des chemins des routes des publications

--- routes/publication.js
+++ routes/publication.js
@@ -6,6 +6,12 @@ const multer = require('../middleware/multer-config');
 
 const publicationCtrl = require('../controllers/publication');
 
+router.get('/', auth, publicationCtrl.getAllPublications);
 router.post('/', auth, multer, publicationCtrl.publish);
+router.get('/:id', auth, publicationCtrl.getOnePublicationByPostId);
+router.put('/:id', auth, multer, publicationCtrl.modifyPublication);
+router.delete('/:id', auth, publicationCtrl.deletePublication);
+
+
 
 module.exports = router;
