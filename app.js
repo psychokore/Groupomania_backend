a1979b4ac4596fe53c9605cc50065e29d06bd2e5
psychokore <juju.faure@yahoo.fr>
2022-06-11 20:16:39 +0200

    route like

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ dotenv.config();
 const userRoutes = require('./routes/user');
 const publicationRoutes = require('./routes/publication');
 const commentRoutes = require('./routes/comment');
+const likeRoutes = require('./routes/like');
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use((req, res, next) => {
   app.use('/api/auth', userRoutes);
   app.use('/api/publication', publicationRoutes);
   app.use('/api/comment', commentRoutes);
+  app.use('/api/like', likeRoutes);
   app.use('/images', express.static(path.join(__dirname, 'images')));
   
   app.use(paginate.middleware(10, 50));
--- routes/like.js
+++ routes/like.js
@@ -5,3 +5,8 @@ const auth = require('../middleware/auth');
 const likeCtrl = require('../controllers/like');
 
 
+router.post('/:id/like', auth, likeCtrl.addLike);
+router.delete('/:id', auth, likeCtrl.deleteLike);
+router.get('/', auth, likeCtrl.getAllLikes);
+
+module.exports = router;
