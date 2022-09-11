9295f8326bae9ae48c25988eff63fc7e901edb31
psychokore <juju.faure@yahoo.fr>
2022-09-11 16:36:45 +0200

    test

--- repository/mysql.js
+++ repository/mysql.js
@@ -1,7 +1,7 @@
 let mysql      = require('mysql');
 let connection = mysql.createConnection({
   host     : process.env.HOST,
-  user     : process.env.USER,
+  user     : process.env.MYSQL_USER,
   password : process.env.PASSWORD,
   database : process.env.DATABASE
 });
